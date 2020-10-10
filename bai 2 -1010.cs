8aad16521b7cd1af4fd5e5a26c93f88b83719549
pvd1302 <pvduc.ns@gmail.com>
2020-10-10 10:47:00 +0700

    ok

---
+++ bai 2 -1010.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_3_1010
+{
+    public class Stack<T>
+    {
+        private int n;
+        private int top;
+        public T[] a;
+        public int Top
+        {
+            get { return top; }
+            set { top = value; }
+        }
+        public Stack(int n)
+        {
+            top = -1;
+            this.n = n;
+            a = new T[n];
+        }
+        public bool isEmpty()
+        {
+            return top == -1;
+        }
+        public bool isFull()
+        {
+            return top == n - 1;
+        }
+        public void Push(T pt)
+        {
+            if (!isFull())
+            {
+                top = top + 1;
+                a[top] = pt;
+            }
+            else
+            {
+                Console.WriteLine("Stack day!");
+            }
+        }
+        public void Pop()
+        {
+            if (!isEmpty())
+            {
+                top = top - 1;
+            }
+            else
+            {
+                Console.WriteLine("Stack rong");
+            }
+        }
+        public void Hienthi()
+        {
+            if (!isEmpty())
+            {
+                for (int i = 0; i <= top; i++)
+                    Console.Write("{0}", a[i].ToString());
+                Console.WriteLine();
+            }
+            else
+                Console.WriteLine("Stack rong roi");
+        }
+    }
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> st = new Stack<int>(5);
+            if (st.isEmpty())
+                Console.WriteLine("Stack rong");
+            Console.WriteLine("**Thuc hien thao tac**");
+            Console.WriteLine("\n");
+            Console.WriteLine("//them {0} phan tu", st.a.Length);
+            st.Push(3);
+            st.Push(4);
+            st.Push(5);
+            st.Push(6);
+            st.Push(7);
+            st.Hienthi();
+            if (st.isFull())
+                Console.WriteLine("Stack da day");
+            Console.WriteLine("\n");
+            Console.WriteLine("//Xoa 1 phan tu");
+            st.Pop();
+            st.Hienthi();
+            Console.WriteLine("\n");
+            //
+            Stack<string> st2 = new Stack<string>(3);
+            if (st2.isEmpty())
+                Console.WriteLine("Stack rong");
+            Console.WriteLine("**Thuc hien thao tac**");
+            Console.WriteLine("\n");
+            Console.WriteLine("//them {0} phan tu", st2.a.Length);
+            st2.Push("a");
+            st2.Push("b");
+            st2.Push("c");
+            st2.Hienthi();
+            if (st2.isFull())
+                Console.WriteLine("Stack da day");
+            Console.WriteLine("\n");
+            Console.WriteLine("//Xoa 3 phan tu");
+            st2.Pop();
+            st2.Pop();
+            st2.Pop();
+            st2.Hienthi();
+            Console.WriteLine("\n");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bai1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace bai_1_1010
+{
+    class diem
+    {
+        private double x;
+        private double y;
+        public diem()
+        {
+            x = 0;
+            y = 0;
+        }
+        public diem(double ox, double oy)
+        {
+            x = ox;
+            y = oy;
+        }
+        public void nhap()
+        {
+            Console.Write("Nhap hoanh do: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Nhap tung do: ");
+            y = double.Parse(Console.ReadLine());
+        }
+        public void Hienthi()
+        {
+            Console.WriteLine("Toa do la : ({0},{1})", x, y);
+        }
+        public double Khoangcach(diem d1, diem d2)
+        {
+            double kc = Math.Sqrt((d1.x - d2.x) * (d1.x - d2.x) + (d1.y - d2.y) * (d1.y - d2.y));
+            return kc;
+        }
+        static void Main(string[] args)
+        {
+            diem t = new diem();
+            t.nhap();
+            t.Hienthi();
+            Console.ReadKey();
+        }
+    }
+}
